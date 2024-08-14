520985e2bab4ae825c924bddb4b0a81280614675
Nicolas Dibarboure <nico.dibarboure@gmail.com>
2024-08-14 17:56:01 -0300

    Agregando paginas de contacto y sobre mi.

--- js/main.js
+++ js/main.js
@@ -238,27 +238,3 @@ document.addEventListener("DOMContentLoaded", () => {
   actualizarTotales();
   actualizarGraficoGastos();
 });
-
-const open = document.querySelector(".container");
-const close = document.querySelector(".close");
-var tl = gsap.timeline({ defaults: { duration: 1, ease: "expo.inOut" } });
-
-open.addEventListener("click", () => {
-  if (tl.reversed()) {
-    tl.play();
-  } else {
-    tl.to("nav", { right: 0, height: 45 })
-      .to("nav", { height: "145dvh" }, "-=.1")
-      .to(
-        "nav ul li a",
-        { opacity: 1, pointerEvents: "all", stagger: 0.2 },
-        "-=.8"
-      )
-      .to(".close", { opacity: 1, pointerEvents: "all" }, "-=.8")
-      .to("nav h1", { opacity: 1 }, "-=1");
-  }
-});
-
-close.addEventListener("click", () => {
-  tl.reverse();
-});
